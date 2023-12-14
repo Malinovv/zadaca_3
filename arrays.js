7db74d49b8831995b6124b087de3a031f6461137
Kristijan Malinov <kristijan.malinovv@gmail.com>
2023-12-14 02:14:48 +0100

    copy the exercises so i can solve

--- arrays.js
+++ arrays.js
@@ -1,14 +1,16 @@
-/*
- * 1. JavaScript arrays can contain a mix of different data types.
- * 2. JavaScript arrays are indexed arrays and so,
- * array elements cannot be accessed using arbitrary strings as indexed, as in associative arrays.
- * 3. JavaScript arrays are zero-indexed: the first element of an array is at index 0,
- * the second is at index 1, and so on - and the last element is at the value of the array's lenght property minus 1.
- * 4. JavaScript arrays can be created using the array constructor and array literal syntax [].
- * (*** or from an array-like object, such as a NodeList or arguments object).
+/**
+ 1. JavaScript arrays can contain a mix of different data types. 
+ 2. JavaScript arrays are indexed arrays and so, 
+ array elements cannot be accessed using arbitrary strings as indexes, as in associative arrays.
+ 3. JavaScript arrays are zero-indexed: the first element of an array is at index 0, 
+ the second is at index 1, and so on — and the last element is at the value of the array's length property 
+ minus 1.
+ 4. JavaScript arrays can be created using the array constructor and array literal syntax []. 
+ (*** or from an array-like object, such as a NodeList or arguments object ***).
  */
 
-let myArray = [1, 2, 3, 4, "five", ["six", "seven", "eight"]];
+let myArray = [1, 2, 3, 4];
+
 let myArray2 = [
   [1, 2],
   [3, 4],
@@ -18,25 +20,37 @@ let myArray2 = [
 myArray[0]; // 1
 myArray[1]; // 2
 
-let pElements = ["<p>1</p>", "<p>2</p>", "<p>2</p>"];
+let pElements = ["<p>1</p>", "<p>2</p>", "<p>3</p>"];
 
-// pElements[1] ......
+// pElements[1].....
 
 // Array advanced concepts:
 // 1. Array methods (ES6): forEach, map, filter and reduce.
 let newArray = [];
-myArray.forEach((element) => {
-  newArray.push(element * 2);
-});
+// let iteratedArray = myArray.forEach((element) => {
+//   newArray.push(element * 2);
+// });
+// let myArray = [1, 2, 3, 4];
 let filteredArray = myArray.filter((element) => {
-  return false;
+  return element > 2;
 });
 let mappedArray = myArray.map((element) => {
   return element + 2;
 });
-console.log(newArray);
-console.log(filteredArray);
-console.log(mappedArray);
+
+// filter numbers greater than 2 and multiple by 3
+let multipledNumbers = myArray
+  .filter((number) => {
+    return number > 2;
+  })
+  .map((number) => {
+    return number * 3;
+  });
+
+let nodes = [{}, {}, {}];
+let filteredNodes = nodes.filter((node) => {});
+
+console.log(multipledNumbers);
 
 let notReducedArr = [
   "The",
@@ -49,10 +63,14 @@ let notReducedArr = [
   "lazy",
   "dog",
 ];
+
 let reducedElement = notReducedArr.reduce((accumulator, element) => {
-  return (accumulator += " " + element);
+  return (accumulator += element);
 }, "");
-// --- double numbers
+
+console.log(reducedElement);
+
+// -- double numbers
 // -- filter even numbers
 // -- calculate total (reduce)
 // -- transform uppercase
@@ -60,4 +78,4 @@ let reducedElement = notReducedArr.reduce((accumulator, element) => {
 // 2. Difference between map and forEach (distructive vs. non-distructive methods)
 // 3. Array sorting
 
-console.log(reducedElement);
+// Homework: reduce tasks from above!
